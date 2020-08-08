8a86cd31667f1fd6d6b1cd1ceb23f7cd0b9cf3a8
Luiz Fernando <luiz-fernando@outlook.com>
2020-08-08 18:25:11 -0300

    Extraído configuração do SECRET_KEY
    close #22

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_296*p%)-+rav71fiv$df%!8r-g1g7##_t!q=n)scboeo)*vo4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
